0f793f8334324e44ad969d92a481d7b20cd3855d
jbraun <jessica.braun@phys.chem.ethz.ch>
2024-08-16 10:34:39 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tabs',
+    description='Torsion Angular Bin Strings',
+    version='0.0.1',
+    py_modules=['tabs'],
+    packages=['tabs'],
+    install_requires=[],
+    entry_points={},
+    author="Jessica Braun",
+    author_email="braunje@ethz.ch",
+)
