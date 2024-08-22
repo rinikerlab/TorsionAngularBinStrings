4fd654767d35840e7d63c9c2b705a73fc5704ac1
jbraun <jessica.braun@phys.chem.ethz.ch>
2024-08-22 15:45:22 +0200

    updated __init.py for plotting API change

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -3,4 +3,4 @@
 from .tabs import GetnTABS, GetTABS, GetTABSMultipleConfs, SortEnsembleByTABS, AnalyzeTABSforIntraRmsd, AnalyzeTABSforInterRmsd
 from .multiplicity import GetMultiplicityAllBonds, AnalyzeMultiplicityContributions, ETKDGv3vsRotBondCheck, CalculateMultiplicityAndBounds
 from .symmetry import GetTABSPermutations, GetSymmetryOrder
-from .plots import PlotOrgDistribution, PlotDihedralDistributions
+from .plots import PlotOrgDistribution, PlotDihedralDistributions, PlotOrgDistributionFitOnly
