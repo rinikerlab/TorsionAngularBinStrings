14f14d5526a69a62febd1310325b71b97e7e2ad1
jbraun <jessica.braun@phys.chem.ethz.ch>
2024-08-28 13:22:58 +0200

    setting release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='tabs',
     description='Torsion Angular Bin Strings',
-    version='0.0.1',
+    version='1.0.0',
     py_modules=['tabs'],
     packages=['tabs'],
     install_requires=[],
