1af0357bd08abf046d3dde7194b91b7763c56514
jbraun <jessica.braun@phys.chem.ethz.ch>
2024-10-15 12:55:35 +0200

    fixing the setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     py_modules=['tabs'],
     install_requires=[],
     entry_points={},
-    packages=find_packages(where="tabs"),
+    packages=find_packages(where="."),
     package_dir={"": "tabs"},
     include_package_data=True,
+    package_dir = {"": "."}
     package_data={'tabs': ['torsionPreferences/*.txt']},
     author="Jessica Braun",
     author_email="braunje@ethz.ch",
