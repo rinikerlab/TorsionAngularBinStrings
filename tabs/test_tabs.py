e701bb423adcc7e0a44caf89c7b2fcbe064ad30d
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-04-10 16:23:55 +0200

    extended testing framework

--- tabs/test_tabs.py
+++ tabs/test_tabs.py
@@ -9,9 +9,14 @@ except ImportError:
 
 class TestTABS(unittest.TestCase):
     mol1 = Chem.AddHs(Chem.MolFromSmiles("OCCCCC=CCCCCO"))
-    mol2 = Chem.AddHs(Chem.MolFromSmiles("C1C(C)CC(CC)CC1"))
+    mol2 = Chem.AddHs(Chem.MolFromSmiles('CC[C@@H]1CCC[C@@H](C)C1'))
     mol3 = Chem.AddHs(Chem.MolFromSmiles("CS(=O)(=O)NCc1nc2cnc3[nH]ccc3c2n1[C@@H]1C[C@H]2CC[C@@H]1C2"))
     mol4 = Chem.AddHs(Chem.MolFromSmiles('CCCc1cc2cc3c(C#C[Si](CC)(CC)CC)c4cc5sc(CCC)cc5cc4c(C#C[Si](CC)(CC)CC)c3cc2s1'))
+    mol5 = Chem.AddHs(Chem.MolFromSmiles('Cc1nc(N)[n+]2c(=O)cc(C)[n-]c2n1'))
+    mol6 = Chem.AddHs(Chem.MolFromSmiles("CC#CC"))
+    mol7 = Chem.AddHs(Chem.MolFromSmiles(r"C/C=C\C"))
+    mol8 = Chem.AddHs(Chem.MolFromSmiles("CC=CC"))
+    mol9 = Chem.AddHs(Chem.MolFromSmiles("C1C(C)CC(CC)CC1"))
 
     def testReporter(self):
         info = DihedralsInfo.FromTorsionLib(self.mol1)
@@ -67,11 +72,45 @@ class TestTABS(unittest.TestCase):
         info = DihedralsInfo.FromTorsionLib(self.mol3)
         nTABS = info.GetnTABS()
         self.assertEqual(nTABS, 96)
+        with self.assertWarnsRegex(UserWarning, "WARNING: No dihedrals found"):
+            info = DihedralsInfo.FromTorsionLib(self.mol6)
+        nTABS = info.GetnTABS()
+        self.assertEqual(nTABS, 1)
 
     def testNotConsideredAtomTypes(self):
-        self.assertWarnsRegex(UserWarning,"WARNING: any torsions with atoms containing anything but H, C, N, O, F, Cl, Br, I, S or P are not considered",\
+        # only tests that there is a warning, not the warning message
+        self.assertWarns(UserWarning,\
                             DihedralsInfo.FromTorsionLib,\
                             self.mol4)
+        # check that the warning message is correct
+        self.assertWarnsRegex(UserWarning,\
+                            "WARNING: any torsions with atoms containing anything but H, C, N, O, F, Cl, Br, I, S or P are not considered.",\
+                            DihedralsInfo.FromTorsionLib,\
+                            self.mol4)
+
+    def testNoTorsionDetected(self):
+        self.assertWarnsRegex(UserWarning,\
+                            "WARNING: No ETKDG torsion library patterns matched",\
+                            DihedralsInfo.FromTorsionLib,\
+                            self.mol5)
+        self.assertWarnsRegex(UserWarning,\
+                            "WARNING: No dihedrals found",\
+                            DihedralsInfo.FromTorsionLib,\
+                            self.mol5)
+        
+    def testStereoEncoding(self):
+        info = DihedralsInfo.FromTorsionLib(self.mol7)
+        self.assertEqual(info.multiplicities, [1])
+        self.assertEqual(info.GetnTABS(),1)
+        info = DihedralsInfo.FromTorsionLib(self.mol8)
+        self.assertEqual(info.multiplicities, [2])
+        self.assertEqual(info.GetnTABS(),2)
+
+    def testUndefinedChiralCenter(self):
+        self.assertWarnsRegex(UserWarning,\
+                            "WARNING: Molecule has chiral centers with undefined stereo",\
+                            DihedralsInfo.FromTorsionLib,\
+                            self.mol9)
 
 if __name__ == '__main__':
     unittest.main()
