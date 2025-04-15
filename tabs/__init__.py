b677db1a105321439f0b1fad233fe82a752459d7
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-04-15 14:01:44 +0200

    removing things from init

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,7 +1,5 @@
-#! /bin/env python
-
-from .plots import PlotOrgDistribution, PlotDihedralDistributions, \
-    PlotOrgDistributionFitOnly, VisualizeEnsemble
+# from .plots import PlotOrgDistribution, PlotDihedralDistributions, \
+    # PlotOrgDistributionFitOnly, VisualizeEnsemble
 from .torsions import DihedralsInfo, TorsionType, GetTorsionProfilesFromMDTraj
-from .fits import ComputeGaussianFit, ComputeTorsionHistograms
-from .application import SortEnsembleByTABS, AnalyzeTABSforInterRmsd, AnalyzeTABSforIntraRmsd
+# from .fits import ComputeGaussianFit, ComputeTorsionHistograms
+# from .application import SortEnsembleByTABS, AnalyzeTABSforInterRmsd, AnalyzeTABSforIntraRmsd
