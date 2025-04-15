55e3fc85b1719296481e9750f1781b8ad0c017a3
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-04-15 15:18:57 +0200

    updates in helpers

--- tabs/helpers.py
+++ tabs/helpers.py
@@ -1,7 +1,7 @@
 from rdkit import Chem
 
 def LoadMultipleConformerSDFile(path,removeHs):
-    suppl = Chem.ForwardSDMolSupplier(path,removeHs=False)
+    suppl = Chem.ForwardSDMolSupplier(path,removeHs=removeHs)
     mol = next(suppl)
     for m in suppl:
         mol.AddConformer(m.GetConformer(),assignId=True)
