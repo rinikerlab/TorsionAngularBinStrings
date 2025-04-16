25dfd9b279a6daf11ab685f1cb7d02dfa44da85b
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-04-16 13:14:06 +0200

    cleaning up init

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,4 +1,4 @@
-from .torsions import DihedralsInfo, TorsionType, GetTorsionProfilesFromMDTraj, FromTorsionLib
+from .torsions import DihedralsInfo, TorsionType, FromTorsionLib
 # from .fits import ComputeGaussianFit, ComputeTorsionHistograms
 # from .application import SortEnsembleByTABS, AnalyzeTABSforInterRmsd, AnalyzeTABSforIntraRmsd
 # from .plots import PlotOrgDistribution, PlotDihedralDistributions, \
