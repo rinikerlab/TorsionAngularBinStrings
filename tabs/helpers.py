a71f6d2e2efeb8fac74bca4233b38b1d78a26e6e
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-04-16 14:56:29 +0200

    tabs/helpers.py was obsolete now

--- tabs/helpers.py
+++
@@ -1,8 +0,0 @@
-from rdkit import Chem
-
-def LoadMultipleConformerSDFile(path,removeHs):
-    suppl = Chem.ForwardSDMolSupplier(path,removeHs=removeHs)
-    mol = next(suppl)
-    for m in suppl:
-        mol.AddConformer(m.GetConformer(),assignId=True)
-    return mol
