dc8834a9b8002ccb93fc2796e7610ed9e5599a17
Niels Maeder <maedern@student.ethz.ch>
2025-10-20 11:11:21 +0200

    add __all__ for * imports

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -4,3 +4,21 @@ from .tabs import GetnTABS, GetTABS, GetTABSMultipleConfs, SortEnsembleByTABS, A
 from .multiplicity import GetMultiplicityAllBonds, AnalyzeMultiplicityContributions, ETKDGv3vsRotBondCheck, CalculateMultiplicityAndBounds
 from .symmetry import GetTABSPermutations, GetSymmetryOrder
 from .plots import PlotOrgDistribution, PlotDihedralDistributions, PlotOrgDistributionFitOnly
+
+__all__ = [
+    "GetnTABS",
+    "GetTABS",
+    "GetTABSMultipleConfs",
+    "SortEnsembleByTABS",
+    "AnalyzeTABSforIntraRmsd",
+    "AnalyzeTABSforInterRmsd",
+    "GetMultiplicityAllBonds",
+    "AnalyzeMultiplicityContributions",
+    "ETKDGv3vsRotBondCheck",
+    "CalculateMultiplicityAndBounds",
+    "GetTABSPermutations",
+    "GetSymmetryOrder",
+    "PlotOrgDistribution",
+    "PlotDihedralDistributions",
+    "PlotOrgDistributionFitOnly",
+]
