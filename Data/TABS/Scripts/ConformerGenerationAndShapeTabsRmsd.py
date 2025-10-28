bff64f12100e88818151f68c70e3c217070b53f3
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-10-28 15:08:51 +0100

    fixed script Data/TABS/Scripts/ConformerGenerationAndShapeTabsRmsd.py

--- Data/TABS/Scripts/ConformerGenerationAndShapeTabsRmsd.py
+++ Data/TABS/Scripts/ConformerGenerationAndShapeTabsRmsd.py
@@ -1,54 +1,20 @@
 """
-This script generates conformers for molecules using the ETKDGv3, calculates the TABS, rmsds and tfds of the ensemble.
+This script generates conformers for molecules using the ETKDGv3, shape aligns the conformers, and calculates the TABS and shape Tanimoto
 """
 
 from rdkit import Chem
-from rdkit.Chem import AllChem, rdDistGeom, rdmolops, TorsionFingerprints, rdMolAlign
+from rdkit.Chem import AllChem, rdDistGeom, rdMolAlign, rdMolTransforms, rdShapeAlign
+from rdkit.Geometry import Point3D
 import pickle
 import uuid
 import yaml
 import time
 import argparse
 import json
-import tabs
 from copy import deepcopy
-
-def GetTFDBetweenAllConformers(mol, useWeights=True, maxDev='equal', symmRadius=2,
-                            ignoreColinearBonds=True):
-    """ Wrapper to calculate the TFD between two list of conformers 
-        of a molecule
-
-        Arguments:
-        - mol:      the molecule of interest
-        - useWeights: flag for using torsion weights in the TFD calculation
-        - maxDev:   maximal deviation used for normalization
-                    'equal': all torsions are normalized using 180.0 (default)
-                    'spec':  each torsion is normalized using its specific
-                            maximal deviation as given in the paper
-        - symmRadius: radius used for calculating the atom invariants
-                    (default: 2)
-        - ignoreColinearBonds: if True (default), single bonds adjacent to
-                                triple bonds are ignored
-                                if False, alternative not-covalently bound
-                                atoms are used to define the torsion
-
-        Return: list of TFD values
-    """
-    confIds1 = [c.GetId() for c in mol.GetConformers()]
-    totalNumConfs = mol.GetNumConformers()
-    tl, tlr = TorsionFingerprints.CalculateTorsionLists(mol, maxDev=maxDev, symmRadius=symmRadius,
-                                  ignoreColinearBonds=ignoreColinearBonds)
-    torsions1 = [TorsionFingerprints.CalculateTorsionAngles(mol, tl, tlr, confId=cid) for cid in confIds1]
-    tfd = []
-    if useWeights:
-        weights = TorsionFingerprints.CalculateTorsionWeights(mol, ignoreColinearBonds=ignoreColinearBonds)
-    else:
-        weights = None
-    for i, t1 in enumerate(torsions1):
-        for j in range(i+1, totalNumConfs):
-            t2 = torsions1[j]
-            tfd.append(TorsionFingerprints.CalculateTFD(t1, t2, weights=weights))
-    return tfd
+from tabs import DihedralInfoFromTorsionLib
+from tqdm import tqdm
+import numpy as np
 
 def GetFailureCauses(counts):
     failureDict = {}
@@ -60,7 +26,6 @@ def ETKDGv3ConformerGeneration(mol, params, uuid_key):
     pickleOutputFilename = f"../Output/{uuid_key}.pkl"
     pickleObject = open(pickleOutputFilename,"wb")
     metadataFilename = f"../Output/{uuid_key}.json"
-
     ps = AllChem.ETKDGv3()
     ps.useBasicKnowledge = params['useBasicKnowledge']
     ps.useSmallRingTorsions = params['useSmallRingTorsions']
@@ -83,25 +48,41 @@ def ETKDGv3ConformerGeneration(mol, params, uuid_key):
     pickleObject.flush()
     with open(metadataFilename,"w") as f:
         json.dump(allMetadata,f)
-    _, allTabs = tabs.GetTABSMultipleConfs(mol)
+    # calculate all TABS
+    info = DihedralInfoFromTorsionLib(mol)
+    allTabs = info.GetTABS()
     molCopy = deepcopy(mol)
     molCopy = Chem.RemoveHs(molCopy)
-    allRmsds = rdMolAlign.GetAllConformerBestRMS(molCopy,numThreads=params['numThreads'])
-    allTfds = GetTFDBetweenAllConformers(molCopy)
-    f = open(f"../Output/{uuid_key}_rmsds.txt","w")
-    g = open(f"../Output/{uuid_key}_tfds.txt","w")
+    f = open(f"../Output/{uuid_key}_metrics.txt","w")
+    f.write("cid1,cid2,tabsAgreement,rmsd,shapeTanimoto,shapeColor\n")
     count = 0
-    for i in range(nOut):
+    confPairs = []
+    for i in tqdm(range(nOut)):
         for j in range(i):
-            if allTabs[i] == allTabs[j]:
-                agreement = 1
-            else:
-                agreement = 0
-            f.write(f"{i},{j},{agreement},{allRmsds[count]}\n")
-            g.write(f"{i},{j},{agreement},{allTfds[count]}\n")
+            confPairs.append([i,j])
+            m1 = Chem.Mol(mol,confId=i)
+            m2 = Chem.Mol(mol,confId=j)
+            m1 = Chem.RemoveAllHs(m1)
+            m2 = Chem.RemoveAllHs(m2)
+            opts1 = rdShapeAlign.ShapeInputOptions()
+            opts2 = rdShapeAlign.ShapeInputOptions()
+            cm1 = m1.GetConformer(i)
+            cm2 = m2.GetConformer(j)
+            tmp1 = []
+            for atom, rank in zip(m1.GetAtoms(), Chem.rdmolfiles.CanonicalRankAtoms(m1, breakTies=False)):
+                tmp1.append((rank, cm1.GetAtomPosition(atom.GetIdx()), 1.0))
+            opts1.customFeatures = tmp1
+            tmp2 = []
+            for atom, rank in zip(m2.GetAtoms(), Chem.rdmolfiles.CanonicalRankAtoms(m2, breakTies=False)):
+                tmp2.append((rank, cm2.GetAtomPosition(atom.GetIdx()), 1.0))
+            opts2.customFeatures = tmp2
+            rdMolTransforms.CanonicalizeConformer(m1.GetConformer())
+            rdMolTransforms.CanonicalizeConformer(m2.GetConformer())
+            tani, color = rdShapeAlign.AlignMol(m1, m2, opts1, opts2, opt_param=0.0, max_preiters=30, max_postiters=30)
+            rmsdShape = rdMolAlign.CalcRMS(m1,m2,i,j)
             count+=1
+            f.write(f"{i},{j},{int(allTabs[i]==allTabs[j])},{rmsdShape},{tani},{color}\n")
     f.close()
-    g.close()
     return 
 
 if __name__ == "__main__":
